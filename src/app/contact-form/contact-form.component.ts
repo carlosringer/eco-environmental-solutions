bb846c4320f842b55b522a3559ef9b23d8a8a214
Ringer <carlosringer@quickenloans.com>
2020-01-17 16:47:03 -0500

    form stuff and circleci config

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,12 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent {
 
-  submitted = false;
-  name: null;
-  email: null;
-  phone: null;
+  constructor() {}
 
-  onSubmit() { this.submitted = true; }
+  public name: string;
+  public email: string;
+  public phone: number;
+  public message: string;
+  public textInput: string;
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    const contactPayload = {
+      name: this.name,
+      email: this.email,
+      phone: this.phone,
+      message: this.message,
+    };
+    console.log(contactPayload);
+
+    const data = JSON.stringify(contactPayload);
+    // fs.writeFileSync('`../submissions/${this.name}.json`', data);
+  }
+
+  toggleInfoModal($event) {
+    console.log($event);
+  }
 
   // TODO: Remove this when we're done
   // get diagnostic() { return JSON.stringify(this.model); }
